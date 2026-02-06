af9863a4a899bf14ebf6dded48a9b0f05a40b2fe
Risham Kumar <rishamwork.18@gmail.com>
2026-02-06 19:55:38 +0530

    Refactor App component by commenting out unused background change logic and update CSS for logout button hover effect

--- login-page/src/App.jsx
+++ login-page/src/App.jsx
@@ -5,28 +5,28 @@ import Home from "./components/Home"
 import Login from "./components/Login"
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 function App() {
-  const [bg, setBg] = useState("light");
+  // const [bg, setBg] = useState("light");
 
-  const changeBg = () => {
-    setBg(bg === "light" ? "dark" : "light");
-  };
+  // const changeBg = () => {
+  //   setBg(bg === "light" ? "dark" : "light");
+  // };
 
-  useEffect(() => {
-    if (bg === "light") {
-      document.body.style.backgroundColor = "white";
-    } else {
-      document.body.style.backgroundColor = "#121212";
-    }
-  }, [bg]);
+  // useEffect(() => {
+  //   if (bg === "light") {
+  //     document.body.style.backgroundColor = "white";
+  //   } else {
+  //     document.body.style.backgroundColor = "#121212";
+  //   }
+  // }, [bg]);
 
   return (
     <>
-    <button
+    {/* <button
         onClick={changeBg}
         style={{ position: "absolute", top: 10, left: 10 }}
       >
         Change Background
-      </button>
+      </button> */}
       <BrowserRouter>
        <Routes>
         <Route path="/" element={<Login/>}/>
